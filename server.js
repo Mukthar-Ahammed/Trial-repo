e8ae28e14bd88681762d9dbf67c8ee082da56888
mukthar <mukthar2004@gmail.com>
2026-02-09 19:51:54 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'dist'), {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-app.get('/{*path}', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
